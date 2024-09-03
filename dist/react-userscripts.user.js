6ef843c61bc8cc3b97c46c86231370972bc1bf44
Juan Carlo Vieri <juan.c.vieri@gmail.com>
2024-09-03 23:18:33 +0800

    Change version.

--- dist/react-userscripts.user.js
+++ dist/react-userscripts.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name     React Userscripts
 // @namespace https://github.com/siefkenj/react-userscripts
-// @version  1.1
+// @version  1.2
 // @description A sample userscript built using react
 // @include *://reboks.nus.edu.sg/*
 // ==/UserScript==
-
 
 (function() {
   "use strict";
