38427433503c10c90f095d57e001cc9f9d7dced3
Juan Carlo Vieri <juan.c.vieri@gmail.com>
2024-09-18 16:25:36 +0800

    Bump up script version.

--- userscript/src/userscript-header.js
+++ userscript/src/userscript-header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name     React Userscripts
 // @namespace https://github.com/siefkenj/react-userscripts
-// @version  1.2
+// @version  1.3
 // @description A sample userscript built using react
 // @include *://reboks.nus.edu.sg/*
 // ==/UserScript==
